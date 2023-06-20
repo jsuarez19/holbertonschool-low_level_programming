a4f9987cdde7337107f566fe0269ec9617db896b
Jose Suarez <jolusuvi@gmail.com>
2023-06-20 12:16:59 -0700

    File uploading...

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	int i, j, k;
+	int aux = 1;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = aux; j <= 8; j++)
+		{
+			for (k = aux + 1; k <= 9 ; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+	
+				if (i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+		aux++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
