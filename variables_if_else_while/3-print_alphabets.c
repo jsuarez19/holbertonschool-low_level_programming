8c7fdd51c33e7a9a40181121bae76492323a4c21
Jose Suarez <jolusuvi@gmail.com>
2023-06-20 10:33:53 -0700

    File uploading...

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	int i;
+	char tmp = 'a';
+
+	for (i = 0; i <= 25; i++)
+	{
+		putchar(tmp);
+		tmp++;
+	}
+
+	tmp = 'A';
+
+	for (i = 0; i <= 25; i++)
+	{
+		putchar(tmp);
+		tmp++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
