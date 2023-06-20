2eb44119cbc0bf1ab00e14120655a833874cc017
Jose Suarez <jolusuvi@gmail.com>
2023-06-20 10:49:28 -0700

    File uploading...

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
