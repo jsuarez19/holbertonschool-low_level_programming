25ceb834f50a7438cc0a3dd1779539834e8c6698
Jose Suarez <jolusuvi@gmail.com>
2023-06-20 12:23:02 -0700

    File uploading...

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -11,23 +11,25 @@ int main(void)
 {
 	int i, j, k;
 	int aux = 1;
+	int aux_2 = 2;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = aux; j <= 8; j++)
 		{
-			for (k = aux + 1; k <= 9 ; k++)
+			for (k = aux_2; k <= 9 ; k++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-	
+
 				if (i != 7)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
+			aux_2++;
 		}
 
 		aux++;
