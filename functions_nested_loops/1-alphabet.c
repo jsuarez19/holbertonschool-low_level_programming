276df938cef0ef57be96bdad3d537b70e80a2f2f
Jose Suarez <jolusuvi@gmail.com>
2023-06-20 18:01:41 -0700

    File uploading...

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,16 @@
 int _putchar(char c)
 {
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
+}
+
+void print_alphabet(void)
+{
+	int i;
+	char letter = 'a';
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(letter);
+		letter++;
+	}
 }
