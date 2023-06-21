dc03549c1d7bd6a9b345dee26eccd453d810b924
Jose Suarez <jolusuvi@gmail.com>
2023-06-21 13:17:44 -0700

    File uploading...

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_to_98 - Prints numbers from n to 98
+ *
+ * @n: number passed to the function
+ *
+ * Return: void on success
+ *
+ */
+
+void print_to_98(int n);
+{
+	int i, a, b;
+
+	for (i = n; i <= 98; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+
+		if (i < 0)
+		{
+			_putchar('-');
+			a = a * -1
+		}
+
+
+		if (a != 0)
+			_putchar(a + '0');
+
+		_putchar(b + '0');
+
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	_putchar('\n');
+}
