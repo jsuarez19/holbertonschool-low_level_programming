52b81e3d19accecac53ca028fc94ab5c1ec940c9
Jose Suarez <jolusuvi@gmail.com>
2023-06-20 17:17:53 -0700

    File uploading...

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	int i;
+	char phrase[] = "_putchar";
+	int length = sizeof(phrase) - 1;
+
+	for (i = 0; i < length; i++)
+		putchar(phrase[i]);
+
+	return (0);
+}
