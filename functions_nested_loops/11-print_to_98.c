49f6ad6460c6d485fe61f29d2a487c0947dcbd53
Jose Suarez <jolusuvi@gmail.com>
2023-06-21 13:46:06 -0700

    File uploading...

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,31 +14,58 @@
 void print_to_98(int n)
 {
 	int i, a, b;
-
-	for (i = n; i <= 98; i++)
+  	
+	if (n > 98)
 	{
-		a = i / 10;
-		b = i % 10;
-
-		if (i < 0)
+		for (i = n; i >= 98; i--)
 		{
-			_putchar('-');
-			a = a * -1;
-			b = b * -1;
-		}
+			a = i / 10;
+			b = i % 10;
 
+			if (i < 0)
+			{
+				_putchar('-');
+				a = a * -1;
+				b = b * -1;
+			}
 
-		if (a != 0)
-			_putchar(a + '0');
+			if (a != 0)
+				_putchar(a + '0');
 
-		_putchar(b + '0');
+			_putchar(b + '0');
 
-		if (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			a = i / 10;
+			b = i % 10;
 
+			if (i < 0)
+			{
+				_putchar('-');
+				a = a * -1;
+				b = b * -1;
+			}
+
+			if (a != 0)
+				_putchar(a + '0');
+
+			_putchar(b + '0');
+
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
 	_putchar('\n');
 }
