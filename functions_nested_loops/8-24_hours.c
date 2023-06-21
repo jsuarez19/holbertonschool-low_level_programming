82eacb6021831d91d3998b67710cd34f09147576
Jose Suarez <jolusuvi@gmail.com>
2023-06-21 11:16:52 -0700

    File uploading...

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * jack_bauer - Prints every minute of a day
+ *
+ * Return: void on success
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if !(i == 2 && j > 3)
+			{
+				for (k = 0; j < 6; j++)
+				{
+					for (l = 0; j < 10; j++)
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+	return (void);
+}
