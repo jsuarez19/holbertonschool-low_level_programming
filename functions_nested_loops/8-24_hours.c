0f894e3ce7468a633e6503406faf605fc4eb47b9
Jose Suarez <jolusuvi@gmail.com>
2023-06-21 11:22:18 -0700

    File uploading...

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -23,11 +23,11 @@ void jack_bauer(void)
 				{
 					for (l = 0; l < 10; l++)
 					{
-						_putchar(i);
-						_putchar(j);
+						_putchar(i + '0');
+						_putchar(j + '0');
 						_putchar(':');
-						_putchar(k);
-						_putchar(l);
+						_putchar(k + '0');
+						_putchar(l + '0');
 						_putchar('\n');
 					}
 				}
