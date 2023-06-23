7d044a40056a2a50e81443f7712d7d2262c4a4f5
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 09:32:59 -0700

    File uploading...

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_numbers- Entry point of the program
+ *
+ * Return: void on success
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
