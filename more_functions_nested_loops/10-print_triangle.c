0a62b604e59f92526917e8e55984232fdbfb9ae1
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 11:41:07 -0700

    File uploading...

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_triangle- Entry point of the program
+ *
+ * @size: number passed to the function
+ *
+ * Return: void on success
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			j = size;
+			k = 1;
+			while (j < i)
+			{
+				_putchar(' ');
+				j--;
+			},
+			while (k <= i)
+			{
+				_putchar('#');
+				k++;
+			}
+
+			_putchar('\n');
+		}
+	}
+}
