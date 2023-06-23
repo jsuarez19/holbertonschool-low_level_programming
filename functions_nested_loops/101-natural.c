b21bbb6e1edd5f2b196bf542052596815942a5c3
Jose Suarez <jolusuvi@gmail.com>
2023-06-22 22:13:32 -0700

    File uploading...

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	int tmp, i;
+	
+	for (i = 1024; i > 0; i--)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			tmp = tmp + i;
+	}
+	printf("%d",tmp);
+	putchar('\n');
+
+	return (0);
+}
