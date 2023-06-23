b409dfc91d785ec8c0ae64422f975d0bcd1e63af
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 13:03:34 -0700

    File uploading...

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	long int num = 612852475143;
+	int prime = 2;
+
+	while (prime != number)
+	{
+		while (num % prime == 0 && num != prime)
+			num = num / prime;
+		prime++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
