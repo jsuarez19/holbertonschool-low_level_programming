81e562747287f60a9a6124752e2063d4a0a2fdb7
Jose Suarez <jolusuvi@gmail.com>
2023-06-22 21:56:11 -0700

    File uploading...

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -15,35 +15,36 @@ void print_times_table(int n)
 {
 	int i, j, a, b, c, num;
 
-	if (n != 15)
+	if (n < 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				num = i * j;
-				if (num >= 100)
-				{
-					a = num / 100;
-					_putchar(a + '0');
-					num = num / 10;
-				}
-				b = num / 10;
 				c = num % 10;
 
-				if (num >= 10)
+				if (num >= 100)
+				{
+					num = num / 10;
+					b = num % 10;
+					a = num / 10;
+					_putchar(a + '0');
 					_putchar(b + '0');
-
+				}
+				if (num >= 10 && num <= 99)
+				{
+					b = num / 10;
+					_putchar(b + '0');
+				}
 				_putchar(c + '0');
 
 				if (j != n)
 				{
 					_putchar(',');
 					_putchar(' ');
-
 					if (i * (j + 1) < 10)
 						_putchar(' ');
-
 					if (i * (j + 1) < 100)
 						_putchar(' ');
 				}
