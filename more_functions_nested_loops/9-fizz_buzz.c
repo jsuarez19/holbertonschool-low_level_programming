54283f4cbc72f17fce271b46044378a09e389cc0
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 11:25:32 -0700

    File uploading...

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d ", i)
+			printf("%d ", i);
 		if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz ")
+			printf("Fizz ");
 		if (i % 3 != 0 && i % 5 == 0)
-			printf("Buzz ")
+			printf("Buzz ");
 		if (i % 15 == 0)
-			printf("FizzBuzz ")
+			printf("FizzBuzz ");
 	}
 
 	_putchar('\n');
