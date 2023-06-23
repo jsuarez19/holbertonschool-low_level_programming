cca6aa1c1cdff1e6401ed75d07d788ab002db1e9
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 11:12:02 -0700

    File uploading...

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_square - Prints a square
+ *
+ * @size: size of the square
+ *
+ * Return: void on success
+ *
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
