5d45de78055fb7d7a78411a1ddbf3366f44001e1
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 09:48:54 -0700

    File uploading...

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void on success
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j, a, b;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			a = j / 10;
+			b = j % 10;
+
+			if(j > 9)
+				_putchar(a + '0');
+			_putchar(b + '0');
+		}
+
+	_putchar('\n');
+	}
+}
