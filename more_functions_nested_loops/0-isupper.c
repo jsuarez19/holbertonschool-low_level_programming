4909098a1512993b17b511cbd42dd203cf3c1998
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 09:18:03 -0700

    File uploading...

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _isupper - checks uppercase character
+ * @c: letter passed to the function
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ *
+ */
+int _islower(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
