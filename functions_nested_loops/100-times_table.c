1d61773dc908f44a6d31b83bc55091ae3b5daeb9
Jose Suarez <jolusuvi@gmail.com>
2023-06-22 21:32:50 -0700

    File uploading...

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -5,6 +5,8 @@
 /**
  * print_times_table - multiplication table
  *
+ * @n: number paased to the function
+ *
  * Return: void on success
  *
  */
@@ -13,34 +15,40 @@ void print_times_table(int n)
 {
 	int i, j, a, b, c, num;
 
-	for (i = 0; i <= n; i++)
+	if (n != 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			num = i * j;
-			a = num / 100;
-			b = num / 10;
-			c = num % 10;
-
-			if (num >= 100)
-				_putchar(a + '0');
-			if (num >= 10)
-				_putchar(b + '0');
-		
-			_putchar(c + '0');
-			
-			if (j != n)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				num = i * j;
+				if (num >= 100)
+				{
+					a = num / 100;
+					_putchar(a + '0');
+					num = num / 10;
+				}
+				b = num / 10;
+				c = num % 10;
 
-				if (i * (j + 1) < 10)
+				if (num >= 10)
+					_putchar(b + '0');
+
+				_putchar(c + '0');
+
+				if (j != n)
+				{
+					_putchar(',');
 					_putchar(' ');
 
-				if (i * (j + 1) < 100)
-					_putchar(' ');
+					if (i * (j + 1) < 10)
+						_putchar(' ');
+
+					if (i * (j + 1) < 100)
+						_putchar(' ');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
