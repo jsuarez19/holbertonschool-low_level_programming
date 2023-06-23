1f24bc2c415db337b879f9e5600e108a2f0e02fb
Jose Suarez <jolusuvi@gmail.com>
2023-06-23 09:36:05 -0700

    File uploading...

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_most_numbers- Entry point of the program
+ *
+ * Return: void on success
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i!= 52)
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
