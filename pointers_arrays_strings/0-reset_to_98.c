89747451afeada3b0ff6bdedadb1f3154ca0c61a
Jose Suarez <jolusuvi@gmail.com>
2023-06-26 10:15:22 -0700

    File uploading...

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#nclude <stdio.h>
+#include <stdio.h>
 #include "main.h"
 /* more headers goes there */
 
@@ -10,7 +10,7 @@
  * Return: 1 if c is a digit, 0 otherwise
  *
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
