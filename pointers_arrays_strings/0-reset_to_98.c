511f7ac234723311983fd6b89ee43ebacde44ccf
Jose Suarez <jolusuvi@gmail.com>
2023-06-26 10:14:17 -0700

    File uploading...

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#nclude <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * reset_to_98 - updates the value to 98
+ *
+ * *n: pointer passed to the function
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ *
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
