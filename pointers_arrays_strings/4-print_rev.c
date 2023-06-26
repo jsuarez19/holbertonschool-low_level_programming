e1b7f62d74444f6a513b46b72a2e265fd82432f3
Jose Suarez <jolusuvi@gmail.com>
2023-06-26 11:13:24 -0700

    File uploading...

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,6 +3,26 @@
 /* more headers goes there */
 
 /**
+ *  * _strlen - returns the length of a string
+ *   *
+ *    * @s: pointer passed to the function
+ *     *
+ *      * Return: integer on success
+ *       */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+/**
  * print_rev - prints a string in reverse
  *
  * @s: pointer passed to the function
