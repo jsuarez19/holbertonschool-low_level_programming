b624852ebc218718691d2cff7739a51a7bf26a44
Jose Suarez <jolusuvi@gmail.com>
2023-06-26 10:54:20 -0700

    File uploading...

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _puts - prints a string
+ *
+ * @str: pointer passed to the function
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + '0');
+		str++;
+	}
+}
