4df7c71f375933c55012d07585085820d6a5eda1
Jose Suarez <jolusuvi@gmail.com>
2023-06-26 21:04:48 -0700

    File uploading...

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,16 +8,21 @@
  * @dest: pointer of the destination
  * @src: pointer of the source
  *
+ * Return: the pointer to dest on success
  */
 char *_strcpy(char *dest, char *src)
 {
+	int length = 1;
+
 	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
 		dest++;
+		length++;
 	}
 	dest++;
 	*dest = '\0';
+	dest -= length;
 	return (dest);
 }
