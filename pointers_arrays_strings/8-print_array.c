b5eb45a262f909f4d47063adc0288d7d8217d8a9
Jose Suarez <jolusuvi@gmail.com>
2023-06-26 20:46:54 -0700

    File uploading...

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_array - prints elements of an array
+ *
+ * @a: pointer passed to the function
+ * @n: number of elements to be printed
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n)
+			printf(", ");
+	}
+	_putchar('\n');
+}
