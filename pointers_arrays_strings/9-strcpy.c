19f76180a9d00942e4acb5605e50f6482e63c729
Jose Suarez <jolusuvi@gmail.com>
2023-06-26 20:59:42 -0700

    File uploading...

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _strcpy - copies a string
+ *
+ * @dest: pointer of the destination
+ * @src: pointer of the source
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while (*scr != '\0')
+	{
+		*dest = *str;
+		scr++;
+		dest++;
+	}
+	dest++;
+	*dest = '\0';
+	return (*dest);
+}
