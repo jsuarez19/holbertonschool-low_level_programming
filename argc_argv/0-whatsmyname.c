cdb68795656207eea32f4bfe2a68246700ad65e7
Jose Suarez <jolusuvi@gmail.com>
2023-06-28 08:44:22 -0700

    File uploading...

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+/**
+ * main - entry point
+ *
+ * Description: prints the name of the program
+ *
+ * @argc: size of *argv[]
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
