8e52f81a29b0ebcc79b45f0ca66bad8286a310c4
Jose Suarez <jolusuvi@gmail.com>
2023-06-30 12:13:32 -0700

    File uploading...

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * _strdup - copies a string
+ *
+ * @str: pointer passed to the function
+ *
+ * Return: a pointer to the new space in memory, NULL if it fails
+ *
+ */
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *p;
+
+	while (*str != '\0')
+	{
+		size++;
+		str++;
+	}
+
+	if (size < 1)
+		return (NULL);
+
+	p = malloc(size);
+
+	if (p == NULL)
+		return (NULL);
+	str -= size + 1;
+	for (i = 0; i < size; i++)
+	{
+		*p = *str;
+		p++;
+	}
+
+	return (p - size);
+}
