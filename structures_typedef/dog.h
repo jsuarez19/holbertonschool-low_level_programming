974fd0f278efa5b46bead115f4042b4da46a3961
Jose Suarez <jolusuvi@gmail.com>
2023-07-03 10:27:21 -0700

    File uploading...

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog{
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - It will keep data from dogs
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the dog's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
