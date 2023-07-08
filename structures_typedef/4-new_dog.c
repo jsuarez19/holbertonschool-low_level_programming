feef2ffbce790b9ccc61a9aab9fb6f8f25e632d3
Jose Suarez <jolusuvi@gmail.com>
2023-07-07 20:45:02 -0700

    File uploading...

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,9 +14,10 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (new_dog != NULL)
 	{
 		new_dog->name = name;
+		new_dog->age = age;
 		new_dog->owner = owner;
 	}
 	else
