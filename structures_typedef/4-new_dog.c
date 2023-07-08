a0206c885e4ba6eb4935eedad2d456a42f6b8d6b
Jose Suarez <jolusuvi@gmail.com>
2023-07-07 20:42:59 -0700

    File uploading...

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: pointer passed to the function
+ * @age: pointer passed to the function
+ * @owner: pointer passed to the function
+ *
+ * Return: NULL if it fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		new_dog->name = name;
+		new_dog->owner = owner;
+	}
+	else
+	{
+		return (NULL);
+}
