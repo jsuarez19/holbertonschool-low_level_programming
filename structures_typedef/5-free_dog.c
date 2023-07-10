665d91816e9195710d011a105c451cf5afbe959d
Jose Suarez <jolusuvi@gmail.com>
2023-07-09 18:17:11 -0700

    File uploading...

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * free_dog - frees dogs
+ *
+ * @d: pointer passed to the function
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d)free(d);
+}
