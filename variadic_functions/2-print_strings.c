1779d77a0f5091865ba34fbb79fd7ecd65fcd3ca
Jose Suarez <jolusuvi@gmail.com>
2023-07-09 21:39:04 -0700

    File uploading...

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (va_arg(ap, char *) != NULL)
-			printf("%d", va_arg(ap, char *));
+			printf("%s", va_arg(ap, char *));
 		else
 			printf("(nil)");
 		if (separator != NULL && n - i != 1)
