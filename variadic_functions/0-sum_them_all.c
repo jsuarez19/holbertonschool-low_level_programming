a7971a5fcc20398f5a83c45b1af3737930a4ca98
Jose Suarez <jolusuvi@gmail.com>
2023-07-09 21:21:00 -0700

    File uploading...

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -17,8 +17,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	int i, sum = 0;
 
+	if (n == 0)
+		return (0);
+
 	va_start(ap, n);
-	for (i = 0;  ; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
 	va_end(ap);
